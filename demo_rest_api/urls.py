8df63c51f5314ee347f588f663895e5d1ff81249
Juan Andres Gualotuna <jgualotu@espol.edu.ec>
2026-01-07 05:57:05 +0000

    "Edición de rutas en urls de demo_rest_api"

--- demo_rest_api/urls.py
+++ demo_rest_api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
    path("index/", views.DemoRestApi.as_view(), name="demo_rest_api_resources" ),
+   
+   path("<str:item_id>/", views.DemoRestApi.as_view(), name="demo_rest_api_item"),
 ]
