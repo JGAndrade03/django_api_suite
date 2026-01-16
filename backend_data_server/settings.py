08de590b03f8f56157cda1f51fb1258ec7ad2047
Diego Bedoya <149976251+DiegoBedoya1@users.noreply.github.com>
2026-01-16 05:05:14 +0000

    hosts

--- backend_data_server/settings.py
+++ backend_data_server/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = "django-insecure-+h&ty0q&f@by1tz1bfww1@+a$==*bfmt%f&-z-okl=2lvb8$jg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['diegobedo.pythonanywhere.com']
 
 
 # Application definition
